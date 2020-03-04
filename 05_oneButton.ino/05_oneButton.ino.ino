f517115864a914eca7c075430c2bbce777646027
alba huerta <huerta.alba2014@gmail.com>
2020-03-03 17:05:27 -0700

    One Button One Flashing Led

--- 05_oneButton.ino/05_oneButton.ino.ino
+++ 05_oneButton.ino/05_oneButton.ino.ino
@@ -1,27 +1,59 @@
 /*
  * Project: Using OneButton library
  * Description: Introduct Students to OneButton Library using a button and the serial monitor
- * Author: 
- * Date: 
+ * Author: Alba Huerta
+ * Date: 3-Mar-20
  */
 
-#include "OneButton.h"
+#include <OneButton.h>
 
 // Setup OneButton on pin 23
+OneButton button1(23, false);
 
-// Create variables buttonState and flash
+int buttonState = LOW;
+int WLed = 4;
+int flash;
+int ledDelay(50);
+
 
 
 void setup() {
 
-// Setup Serial Monitor
-// Link oneButton library to functions click1, doubleclick1, longPressStart1
-oneButton button1(23,false);
-} 
+ Serial.begin(9600);
+ while (!Serial);
+
+ //link the button 1 fucntions.
+ button1.attachClick(click1);
+ button1.attachDoubleClick(doubleclick1);
+ /*button1.attachLongPressStart(longPresStart1);
+ button1.attachLongPressStop(longPressStop1);*/
+ button1.setClickTicks(250);
+ button1.setPressTicks(2000);
+ pinMode(WLed, OUTPUT);
+ 
+ 
+ if(Serial) Serial.println("Starting OneButton...");
+
+}
 
 void loop() {
   // keep watching the push buttons:
   button1.tick();
+  if (buttonState == LOW){
+  digitalWrite(WLed, LOW);
+}
+else
+{
+  if (flash == LOW){
+  digitalWrite(WLed, HIGH);}
+  else{
+  digitalWrite(WLed, HIGH);
+  delay(50);
+  digitalWrite(WLed, LOW);
+  delay(50);}}
+  
+  button1.tick();
+ 
 } // loop
 
 
@@ -29,14 +61,20 @@ void loop() {
 
 // This function will be called when the button1 was pressed 1 time.
 void click1() {
-//change and print buttonState
-  
-} // click1
+  Serial.println("Button 1 click.");
+  buttonState = !buttonState;
+  Serial.print("buttonState =");
+  Serial.println(buttonState);
+
+  } // click1
 
 
 // This function will be called when the button1 was pressed 2 times in a short timeframe.
 void doubleclick1() {
-// change state of flash and print
+  Serial.println("Button 1 doubleclick.");
+  flash = !flash;
+  Serial.print("flash = ");
+  Serial.println(flash);
 } // doubleclick1
 
 
